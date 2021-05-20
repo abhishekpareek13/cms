b5f257e0ad0ddc29dfb85bee76a4e27aaf693460
Abhishek Pareek <abhihekpareek13@gmail.com>
2021-05-20 20:15:14 +0530

    Initial Commit

---
+++ src/test/java/com/sample/abhishek/firstms/FirstmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.abhishek.firstms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
